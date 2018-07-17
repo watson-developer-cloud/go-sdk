c81e07ed571416b488578d9db13da7ca0c263209
Michelle Zeitlin <Michelle.Zeitlin@ibm.com>
2018-07-17 15:46:57 -0500

    Added examples folder with ListWorkspaces example

---
+++ examples/assistantV1.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	watson "golang-sdk"
+	"golang-sdk/assistantV1"
+)
+
+func main() {
+	// Instantiate the Watson Assistant service
+	assistant, assistantErr := assistantV1.NewAssistantV1(watson.Credentials{
+		ServiceURL: "YOUR SERVICE URL",
+		Version: "2018-02-16",
+		Username: "YOUR SERVICE USERNAME",
+		Password: "YOUR SERVICE PASSWORD",
+	})
+
+	// Check successful instantiation
+	if assistantErr != nil {
+		fmt.Println(assistantErr)
+		return
+	}
+
+	// Call the assistant ListWorkspaces method
+	list, listErr := assistant.ListWorkspaces(0, true, "", "", false)
+
+	// Check successful call
+	if listErr != nil {
+		fmt.Println(listErr)
+		return
+	}
+
+	// Cast response from call to the specific struct returned by ListWorkspacesResponse
+	// NOTE: other than DELETE requests, every method has a corresponding <methodName>Response() function
+	result := list.ListWorkspacesResponse()
+
+	// Check successful casting
+	if result != nil {
+		// Print result
+		fmt.Printf("FOUND %v WORKSPACES\n", len(result.Workspaces))
+		fmt.Println(result)
+	}
+}
