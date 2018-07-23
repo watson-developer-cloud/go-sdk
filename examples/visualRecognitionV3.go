1365144d4b2ce752331b3034c91ffc2f86d85284
Vaishali Jayaraman <vaishalijayaramanibm.com@vaishalis-mbp.austin.ibm.com>
2018-07-23 14:16:18 -0500

    Modified visual recognition (+2 squashed commit)
    Squashed commit:
    
    [3bc6afb] Modified visual recognition example
    
    [25d262c] Added visual recognition example

---
+++ examples/visualRecognitionV3.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	watson "golang-sdk"
+	"golang-sdk/visualRecognitionV3"
+	"os"
+)
+
+func main() {
+	// Instantiate the Watson Visual Recognition service
+	visualRecognition, visualRecognitionErr := visualRecognitionV3.NewVisualRecognitionV3(watson.Credentials{
+		ServiceURL: "YOUR SERVICE URL",
+		Version: "2018-03-19",
+		APIkey: "YOUR SERVICE API KEY",
+	})
+
+	// Check successful instantiation
+	if visualRecognitionErr != nil {
+		fmt.Println(visualRecognition)
+		return
+	}
+	
+	/* CLASSIFY */
+
+	// Read file with image to classify
+	pwd, _ := os.Getwd()
+	imageFile, imageFileErr := os.Open(pwd + "/resources/visualRecognition.jpg")
+
+	// Check successful file read
+	if imageFileErr != nil {
+		fmt.Println(imageFileErr)
+		return
+	}
+
+	// Call the visual recognition Classify  method
+	classify, classifyErr := visualRecognition.Classify(*imageFile, "en", "https://www.readersdigest.ca/wp-content/uploads/2011/01/4-ways-cheer-up-depressed-cat.jpg", 0.5, []string {"IBM"}, []string{"default"}, "JPEG")
+
+	// Check successful call
+	if classifyErr != nil {
+		fmt.Println(classifyErr)
+		return
+	}
+
+	// Cast response from call to the specific struct returned by GetClassifyResult
+	// NOTE: other than DELETE requests, every method has a corresponding Get<methodName>Result() function
+	classifyResult := visualRecognitionV3.GetClassifyResult(classify)
+
+	// Check successful casting
+	if classifyResult != nil {
+		// Print result
+		fmt.Println(classifyResult)
+	}
+
+}
