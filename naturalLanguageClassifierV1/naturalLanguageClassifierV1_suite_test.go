df9196d46c236cf56280a845f805ab41fd3a1a29
Michelle Zeitlin <Michelle.Zeitlin@ibm.com>
2018-08-09 16:29:28 -0500

    Added generated unit tests for NaturalLanguageClassifierV1 (45.6% coverage)

---
+++ naturalLanguageClassifierV1/naturalLanguageClassifierV1_suite_test.go
@@ -0,0 +1,13 @@
+package naturalLanguageClassifierV1_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestNaturalLanguageClassifierV1(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "NaturalLanguageClassifierV1 Suite")
+}
