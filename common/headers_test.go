13069db49615fd46c10c2b5b9ea572cd31095791
Phil Adams <phil_adams@us.ibm.com>
2019-03-12 16:41:29 -0500

    feat(common): Added common package with GetSdkHeaders() impl

---
+++ common/headers_test.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
+)
+
+func TestGetSystemInfo(t *testing.T) {
+	var sysinfo = GetSystemInfo()
+	assert.NotNil(t, sysinfo)
+	assert.True(t, strings.Contains(sysinfo, "arch="))
+	assert.True(t, strings.Contains(sysinfo, "os="))
+	assert.True(t, strings.Contains(sysinfo, "go.version="))
+}
+
+func TestGetSdkHeaders(t *testing.T) {
+	var headers = GetSdkHeaders("myService", "v123", "myOperation")
+	assert.NotNil(t, headers)
+
+	var foundIt bool
+	_, foundIt = headers[HEADER_SDK_ANALYTICS]
+	assert.True(t, foundIt)
+
+	_, foundIt = headers[HEADER_USER_AGENT]
+	assert.True(t, foundIt)
+}
