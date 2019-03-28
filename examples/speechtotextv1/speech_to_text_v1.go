ec5b5b934481c634149bd9245ed38f306399ce49
Erika-Dsouza <erika.dsouza@ibm.com>
2019-03-28 18:37:14 -0400

    feat(STT): Recognize using websockets, thanks to @theThinkingCap

--- examples/speechtotextv1/speech_to_text_v1.go
+++ examples/speechtotextv1/speech_to_text_v1.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"os"
-
+	"encoding/json"
+	"fmt"
 	"github.com/IBM/go-sdk-core/core"
 	"github.com/watson-developer-cloud/go-sdk/speechtotextv1"
+	"os"
 )
 
 func main() {
@@ -51,4 +52,42 @@ func main() {
 	if recognizeResult != nil {
 		core.PrettyPrint(recognizeResult, "Recognize")
 	}
+
+	// Example using websockets!
+	audio, _ = os.Open(pwd + "/../../resources/audio_example.mp3")
+
+	// callbook can have `OnOpen`, `onData`, `OnClose` and `onError` functions
+	callback := myCallBack{}
+
+	recognizeUsingWebsocketOptions := service.
+		NewRecognizeUsingWebsocketOptions(audio, "audio/mp3")
+
+	recognizeUsingWebsocketOptions.
+		SetModel("en-US_BroadbandModel").
+		SetWordConfidence(true).
+		SetSpeakerLabels(true).
+		SetTimestamps(true)
+
+	service.RecognizeUsingWebsockets(recognizeUsingWebsocketOptions, callback)
+}
+
+type myCallBack struct{}
+
+func (cb myCallBack) OnOpen() {
+	fmt.Println("Handshake successful")
+}
+
+func (cb myCallBack) OnClose() {
+	fmt.Println("Closing connection")
+}
+
+func (cb myCallBack) OnData(resp *core.DetailedResponse) {
+	var speechResults speechtotextv1.SpeechRecognitionResults
+	result := resp.GetResult().([]byte)
+	json.Unmarshal(result, &speechResults)
+	core.PrettyPrint(speechResults, "Recognized audio: ")
+}
+
+func (cb myCallBack) OnError(err error) {
+	panic(err)
 }
