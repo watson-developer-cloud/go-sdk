38baa14545ffd6f5fb750b6c328bcc9cb3ae8d1c
ehdsouza <erika27desouza@gmail.com>
2020-01-17 15:52:14 -0500

    refacot(nlu) regenerate deprecation message

--- naturallanguageunderstandingv1/natural_language_understanding_v1.go
+++ naturallanguageunderstandingv1/natural_language_understanding_v1.go
@@ -521,9 +521,14 @@ type CategoriesOptions struct {
 	// Maximum number of categories to return.
 	Limit *int64 `json:"limit,omitempty"`
 
-	// Deprecated: The custom categories experimental feature will be retired on 19 December 2019. On that date,
-	// deployed custom categories models will no longer be accessible in Natural Language Understanding.
-	// The feature will be removed from Knowledge Studio on an earlier date.
+	// Enter a [custom
+	// model](https://cloud.ibm.com/docs/services/natural-language-understanding?topic=natural-language-understanding-customizing)
+	// ID to override the standard categories model.
+	//
+	// The custom categories experimental feature will be retired on 19 December 2019. On that date, deployed custom
+	// categories models will no longer be accessible in Natural Language Understanding. The feature will be removed from
+	// Knowledge Studio on an earlier date. Custom categories models will no longer be accessible in Knowledge Studio on 17
+	// December 2019.
 	Model *string `json:"model,omitempty"`
 }
 
