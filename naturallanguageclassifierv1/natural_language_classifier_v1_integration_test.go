0e0835a698e5c92008ec585eef96d04a26e4d5ee
Phil Adams <phil_adams@us.ibm.com>
2020-04-14 13:45:35 -0500

    test(naturallanguageclassifierv1): improve test to exercise classify operation

--- naturallanguageclassifierv1/natural_language_classifier_v1_integration_test.go
+++ naturallanguageclassifierv1/natural_language_classifier_v1_integration_test.go
@@ -3,7 +3,7 @@
 package naturallanguageclassifierv1_test
 
 /**
- * (C) Copyright IBM Corp. 2018, 2019.
+ * (C) Copyright IBM Corp. 2018, 2020.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package naturallanguageclassifierv1_test
  */
 
 import (
+	"encoding/json"
 	"net/http"
 	"os"
 	"testing"
@@ -29,98 +30,156 @@ import (
 	"github.com/watson-developer-cloud/go-sdk/naturallanguageclassifierv1"
 )
 
+const skipMessage = "External configuration could not be loaded, skipping..."
+
+var configLoaded bool
+var configFile = "../.env"
+
 var service *naturallanguageclassifierv1.NaturalLanguageClassifierV1
-var serviceErr error
-var classifier *naturallanguageclassifierv1.Classifier
-
-func init() {
-	err := godotenv.Load("../.env")
-
-	if err == nil {
-		service, serviceErr = naturallanguageclassifierv1.
-			NewNaturalLanguageClassifierV1(&naturallanguageclassifierv1.NaturalLanguageClassifierV1Options{})
-
-		if serviceErr == nil {
-			customHeaders := http.Header{}
-			customHeaders.Add("X-Watson-Learning-Opt-Out", "1")
-			customHeaders.Add("X-Watson-Test", "1")
-			service.Service.SetDefaultHeaders(customHeaders)
-		}
-	}
-}
+var testClassifierID string
+var availableClassifierID string
 
 func shouldSkipTest(t *testing.T) {
-	if service == nil {
-		t.Skip("Skipping test as service credentials are missing")
+	if !configLoaded {
+		t.Skip(skipMessage)
 	}
 }
-func TestClassifier(t *testing.T) {
+
+func TestLoadConfig(t *testing.T) {
+	err := godotenv.Load(configFile)
+	if err != nil {
+		t.Skip(skipMessage)
+	} else {
+		configLoaded = true
+	}
+}
+
+func TestConstructService(t *testing.T) {
+	shouldSkipTest(t)
+
+	var err error
+
+	serviceOptions := &naturallanguageclassifierv1.NaturalLanguageClassifierV1Options{}
+	service, err = naturallanguageclassifierv1.NewNaturalLanguageClassifierV1(serviceOptions)
+	assert.Nil(t, err)
+	assert.NotNil(t, service)
+
+	if err == nil {
+		customHeaders := http.Header{}
+		customHeaders.Add("X-Watson-Learning-Opt-Out", "1")
+		customHeaders.Add("X-Watson-Test", "1")
+		service.Service.SetDefaultHeaders(customHeaders)
+	}
+}
+
+func TestCreateClassifier(t *testing.T) {
 	shouldSkipTest(t)
 
 	// create classifier
-	pwd, _ := os.Getwd()
-
-	metadata, metadataErr := os.Open(pwd + "/../resources/weather_training_metadata.json")
+	metadata, metadataErr := os.Open("../resources/weather_training_metadata.json")
 	assert.Nil(t, metadataErr)
 
-	trainingData, trainingDataErr := os.Open(pwd + "/../resources/weather_training_data.csv")
+	trainingData, trainingDataErr := os.Open("../resources/weather_training_data.csv")
 	assert.Nil(t, trainingDataErr)
 
-	createClassifier, _, responseErr := service.CreateClassifier(
+	classifier, response, responseErr := service.CreateClassifier(
 		&naturallanguageclassifierv1.CreateClassifierOptions{
 			TrainingData:     trainingData,
 			TrainingMetadata: metadata,
 		},
 	)
 	assert.Nil(t, responseErr)
-	assert.NotNil(t, createClassifier)
+	assert.NotNil(t, response)
+	assert.NotNil(t, classifier)
+	assert.Equal(t, "Training", *classifier.Status)
 
-	// List classifier
-	listClassifer, _, responseErr := service.ListClassifiers(
-		&naturallanguageclassifierv1.ListClassifiersOptions{},
-	)
-	assert.Nil(t, responseErr)
-	assert.NotNil(t, listClassifer)
+	testClassifierID = *classifier.ClassifierID
+}
+
+func TestGetClassifier(t *testing.T) {
+	shouldSkipTest(t)
+
+	if testClassifierID == "" {
+		t.Skip("Classifier ID not available, skipping test")
+	}
 
 	// Get classifier
-	getClassifer, _, responseErr := service.GetClassifier(
+	classifier, response, responseErr := service.GetClassifier(
 		&naturallanguageclassifierv1.GetClassifierOptions{
-			ClassifierID: createClassifier.ClassifierID,
+			ClassifierID: &testClassifierID,
 		},
 	)
 	assert.Nil(t, responseErr)
-	assert.NotNil(t, getClassifer)
+	assert.NotNil(t, response)
+	assert.NotNil(t, classifier)
+	if classifier != nil {
+		b, _ := json.MarshalIndent(classifier, "", "  ")
+		t.Log("GetClassifier response: ", string(b))
+	}
+}
 
-	classifier = createClassifier
+func TestListClassifiers(t *testing.T) {
+	shouldSkipTest(t)
+
+	// List classifier
+	classifierList, response, responseErr := service.ListClassifiers(
+		&naturallanguageclassifierv1.ListClassifiersOptions{},
+	)
+	assert.Nil(t, responseErr)
+	assert.NotNil(t, response)
+	assert.NotNil(t, classifierList)
+	if classifierList != nil {
+		// Now fetch each classifier (to get the status) and find the first one
+		// with a status of "Available".
+		for _, c := range classifierList.Classifiers {
+			t.Logf("Fetching classifier id=%s\n", *c.ClassifierID)
+			classifier, _, err := service.GetClassifier(
+				&naturallanguageclassifierv1.GetClassifierOptions{
+					ClassifierID: c.ClassifierID,
+				},
+			)
+			if err == nil {
+				t.Logf("   status=%s\n", *classifier.Status)
+				if *classifier.Status == naturallanguageclassifierv1.Classifier_Status_Available {
+					availableClassifierID = *classifier.ClassifierID
+					t.Logf("Found available classifier id: %s\n", availableClassifierID)
+					break
+				}
+			}
+		}
+	}
 }
 
 func TestClassify(t *testing.T) {
 	shouldSkipTest(t)
 
-	// classify
-	if *classifier.Status != "Available" {
-		t.Skip("Skip test classify")
+	if availableClassifierID == "" {
+		t.Skip("Available classifier not found, skipping test...")
 	}
-	classify, _, responseErr := service.Classify(
+
+	// classify
+	classification, response, responseErr := service.Classify(
 		&naturallanguageclassifierv1.ClassifyOptions{
-			ClassifierID: classifier.ClassifierID,
+			ClassifierID: &availableClassifierID,
 			Text:         core.StringPtr("How hot will it be today?"),
 		},
 	)
 	assert.Nil(t, responseErr)
-	assert.NotNil(t, classify)
+	assert.NotNil(t, response)
+	assert.NotNil(t, classification)
 }
 
 func TestClassifyCollection(t *testing.T) {
 	shouldSkipTest(t)
 
-	if *classifier.Status != "Available" {
-		t.Skip("Skip test classify collection.")
+	if availableClassifierID == "" {
+		t.Skip("Available classifier not found, skipping test...")
 	}
+
 	// classify collection
-	classifyCollection, _, responseErr := service.ClassifyCollection(
+	classificationCollection, response, responseErr := service.ClassifyCollection(
 		&naturallanguageclassifierv1.ClassifyCollectionOptions{
-			ClassifierID: classifier.ClassifierID,
+			ClassifierID: &availableClassifierID,
 			Collection: []naturallanguageclassifierv1.ClassifyInput{
 				naturallanguageclassifierv1.ClassifyInput{
 					Text: core.StringPtr("How hot will it be today?"),
@@ -132,16 +191,21 @@ func TestClassifyCollection(t *testing.T) {
 		},
 	)
 	assert.Nil(t, responseErr)
-	assert.NotNil(t, classifyCollection)
+	assert.NotNil(t, response)
+	assert.NotNil(t, classificationCollection)
 }
 
 func TestDeleteClassifier(t *testing.T) {
 	shouldSkipTest(t)
 
+	if testClassifierID == "" {
+		t.Skip("No classifier to delete, skipping test...")
+	}
+
 	// Delete classifier
 	response, responseErr := service.DeleteClassifier(
 		&naturallanguageclassifierv1.DeleteClassifierOptions{
-			ClassifierID: classifier.ClassifierID,
+			ClassifierID: &testClassifierID,
 		},
 	)
 	assert.Nil(t, responseErr)
