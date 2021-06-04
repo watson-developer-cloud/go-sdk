e43a81cc1f4eeebc218aa3e7975db4b7f22a3bfb
repjarms <jeffarn12@gmail.com>
2021-06-04 09:14:43 -0400

    chore(copyright dates): generate copyright dates

--- v2/naturallanguageclassifierv1/natural_language_classifier_v1_integration_test.go
+++ v2/naturallanguageclassifierv1/natural_language_classifier_v1_integration_test.go
@@ -3,7 +3,7 @@
 package naturallanguageclassifierv1_test
 
 /**
- * (C) Copyright IBM Corp. 2018, 2020.
+ * (C) Copyright IBM Corp. 2018, 2021.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
