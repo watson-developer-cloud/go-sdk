a4b9b4beae054b2547d3b98e3b89d82d03d23009
repjarms <jeffarn12@gmail.com>
2022-03-18 11:40:01 -0400

    chore(copyright dates): update copyright dates

--- v3/discoveryv2/discovery_v2_examples_test.go
+++ v3/discoveryv2/discovery_v2_examples_test.go
@@ -2,7 +2,7 @@
 // +build examples
 
 /**
- * (C) Copyright IBM Corp. 2021.
+ * (C) Copyright IBM Corp. 2021, 2022.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
